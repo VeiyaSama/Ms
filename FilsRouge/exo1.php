28b04409ff24e209eda4d7fd649f3c7497dc4529
djo afpa <djon147@hotmail.com>
2023-07-06 10:07:35 +0200

    Exophp

---
+++ FilsRouge/exo1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a = 500;
+$b = 1;
+$c = 150;
+$d = 1;
+
+
+while ($c>$b){
+    echo "$b\n";
+    
+    $b=$b+2;
+    }
+while ($a=$d)
+    echo "Je dois faire des sauvegardes régulièrement";
+    $d=$d+1;
+?>
+
+</body>
+</html>
