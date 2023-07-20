c52c3dbb4a4a7a004853f04cdd82f349e74a22b7
djo afpa <djon147@hotmail.com>
2023-07-20 13:28:10 +0200

    médiaquerries

--- FilsRouge/contact.php
+++ FilsRouge/contact.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Formulaire Bootstrap</title>
+  <title>Fils Rouge </title>
   <link rel="stylesheet" href="../FilsRouge/assets/style.css">
   <!-- Inclure le fichier CSS de Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
     <header>
